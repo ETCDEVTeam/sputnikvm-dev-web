3cdd9d28dad3f90ac39c2822b19b0ced6a1e0197
Wei Tang <hi@that.world>
2017-10-03 18:59:36 +0800

    Add css reference in Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 import Reflex
 import Reflex.Dom
-main = mainWidget $ text "Hello, world!"
+import qualified Data.Map as Map
+
+main :: IO ()
+main = mainWidgetWithHead headElement bodyElement
+
+headElement :: MonadWidget t m => m ()
+headElement = do
+  el "title" (text "SputnikVM Development Environment")
+  styleSheet "css/style.css"
+  where
+    styleSheet link = elAttr "link" (Map.fromList [
+                                        ("rel", "stylesheet")
+                                        , ("type", "text/css")
+                                        , ("href", link)
+                                        ]) $ return ()
+
+bodyElement :: MonadWidget t m => m ()
+bodyElement = el "h1" (text "Hello, world!")
